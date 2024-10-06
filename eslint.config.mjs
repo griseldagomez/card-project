9a0c6aca3468f4fc4ee2be11ca06520607fe983b
shadowc <info@matiasjose.com>
2024-10-07 00:34:42 +0200

    new eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,32 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
-  {files: ["assets/**/*.{js,mjs,cjs,ts,jsx,tsx}"], ignores: ['assets/vendor/*.*']},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    {
+        files: ['assets/**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    },
+    {
+        ignores: [
+            'vendor/**/*.{js,ts,mjs,cjs,jsx,tsx}',
+            'assets/vendor/**/*.js',
+            'assets/controllers/**/*.js',
+        ],
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+    },
+    eslintConfigPrettier,
+    eslintPluginPrettierRecommended,
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
 ];
