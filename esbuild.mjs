0f211f67f24ae7e49dcac6a596532868b16772ec
shadowc <info@matiasjose.com>
2024-10-07 00:17:58 +0200

    install node modules, new eslint, bin scripts

---
+++ esbuild.mjs
@@ -0,0 +1,52 @@
+import { build } from 'esbuild';
+import { clean } from 'esbuild-plugin-clean';
+import { sassPlugin } from 'esbuild-sass-plugin';
+import copy from 'esbuild-copy-files-plugin';
+
+await build({
+    entryPoints: ['./assets/scss/app.scss'],
+    bundle: true,
+    minify: true,
+    outdir: './assets/styles',
+    loader: {
+        '.eot': 'file',
+        '.ttf': 'file',
+        '.png': 'file',
+        '.webp': 'file',
+        '.woff': 'file',
+        '.woff2': 'file',
+        '.svg': 'file',
+    },
+    assetNames: '[dir]/[name]',
+    plugins: [
+        clean({
+            patterns: ['./assets/styles/*', './assets/controllers/*'],
+        }),
+        // {
+        //     name: 'resolveImages',
+        //     setup(bld) {
+        //         // Mark all paths starting with "/bundles/admin/images" as external
+        //         bld.onResolve(
+        //             { filter: /^\.\/images\// },
+        //             (args) => ({ path: args.path.replace('./images', '../images'), external: true }),
+        //         );
+        //     },
+        // },
+        {
+            name: 'resolveFonts',
+            setup(bld) {
+                // Mark all paths starting with "./fonts" as external and replace it with correct path
+                bld.onResolve({ filter: /^\.\/fonts\// }, (args) => ({
+                    path: args.path.replace('./fonts', '../fonts'),
+                    external: true,
+                }));
+            },
+        },
+        copy({
+            source: './node_modules/bootstrap-icons/font/fonts',
+            target: './assets/fonts',
+            copyWithFolder: false,
+        }),
+        sassPlugin(),
+    ],
+});
---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {files: ["assets/**/*.{js,mjs,cjs,ts,jsx,tsx}"], ignores: ['assets/vendor/*.*']},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
