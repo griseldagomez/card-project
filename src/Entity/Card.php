142fac01dc161e9c6b959f995dbd05cbb72ada45
shadowc <info@matiasjose.com>
2024-10-07 17:43:22 +0200

    Add validation, fix card entity

--- src/Entity/Card.php
+++ src/Entity/Card.php
@@ -6,6 +6,7 @@ use App\Repository\CardRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use ShadowC\AdminBundle\Attribute\ListField;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CardRepository::class)]
 class Card
@@ -17,65 +18,41 @@ class Card
 
     #[ORM\Column(length: 255)]
     #[ListField(sortable: true, searchable: true)]
-    private ?string $title = null;
-
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $text = null;
-
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $project_image = null;
+    #[Assert\NotNull]
+    private ?string $name = null;
 
     #[ORM\Column(length: 255)]
+    #[ListField(sortable: true, searchable: true)]
+    #[Assert\NotNull]
+    private ?string $job = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Url]
     private ?string $github_url = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Url]
+    private ?string $linkedin_url = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Email]
     private ?string $email = null;
 
     #[ORM\Column(length: 40)]
+    #[Assert\NotNull]
     private ?string $phone = null;
 
     #[ORM\Column(length: 6)]
+    #[Assert\NotNull]
     private ?string $color_scheme = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotNull]
+    private ?string $photo = null;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): static
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    public function getText(): ?string
-    {
-        return $this->text;
-    }
-
-    public function setText(string $text): static
-    {
-        $this->text = $text;
-
-        return $this;
-    }
-
-    public function getProjectImage(): ?string
-    {
-        return $this->project_image;
-    }
-
-    public function setProjectImage(string $project_image): static
-    {
-        $this->project_image = $project_image;
-
-        return $this;
     }
 
     public function getGithubUrl(): ?string
@@ -122,6 +99,54 @@ class Card
     public function setColorScheme(string $color_scheme): static
     {
         $this->color_scheme = $color_scheme;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): static
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getJob(): ?string
+    {
+        return $this->job;
+    }
+
+    public function setJob(?string $job): static
+    {
+        $this->job = $job;
+
+        return $this;
+    }
+
+    public function getLinkedinUrl(): ?string
+    {
+        return $this->linkedin_url;
+    }
+
+    public function setLinkedinUrl(?string $linkedin_url): static
+    {
+        $this->linkedin_url = $linkedin_url;
 
         return $this;
     }
