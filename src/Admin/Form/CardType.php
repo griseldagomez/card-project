4417871ede5f92f351cbba289a915cfa647d7a57
shadowc <info@matiasjose.com>
2024-10-08 01:26:56 +0200

    Fix routes and finish card form

--- src/Admin/Form/CardType.php
+++ src/Admin/Form/CardType.php
@@ -4,6 +4,7 @@ namespace App\Admin\Form;
 
 use App\Entity\Card;
 use ShadowC\AdminBundle\Form\BaseCRUDType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,12 +15,41 @@ class CardType extends BaseCRUDType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class, [
-                'label' => 'form.title.label',
+            ->add('colorScheme', ChoiceType::class, [
+                'label' => 'form.colorScheme.label',
+                'required' => true,
+                'choices' => [
+                    'Color 1' => 'color1',
+                    'Color 2' => 'color2',
+                    'Color 3' => 'color3',
+                ],
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'form.name.label',
                 'required' => true,
             ])
-            ->add('text', TextareaType::class, [
-                'label' => 'form.text.label',
+            ->add('job', TextType::class, [
+                'label' => 'form.job.label',
+                'required' => true,
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'form.email.label',
+                'required' => true,
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'form.phone.label',
+                'required' => true,
+            ])
+            ->add('linkedinUrl', TextType::class, [
+                'label' => 'form.linkedinUrl.label',
+                'required' => true,
+            ])
+            ->add('githubUrl', TextType::class, [
+                'label' => 'form.githubUrl.label',
+                'required' => true,
+            ])
+            ->add('photo', TextareaType::class, [
+                'label' => 'form.photo.label',
                 'required' => true,
             ])
         ;
